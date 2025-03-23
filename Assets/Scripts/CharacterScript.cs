e208b15d4df5271aca3ab747081fd761caa3fd57
Blechman Shy (NorthStar) <rakesh1985@gmail.com>
2025-03-23 19:43:58 +0200

    Build errors

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -1,12 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-//using System.Diagnostics;
 using System.Linq;
-using System.Security.Cryptography;
-using Unity.Mathematics;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
