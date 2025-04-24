ea253a8b3e142479c5b6a4ad72e266826b027956
Blechman Shy (NorthStar) <rakesh1985@gmail.com>
2025-04-24 20:18:16 +0300

    New build

--- Assets/Scripts/PopUpsScript.cs
+++ Assets/Scripts/PopUpsScript.cs
@@ -7,31 +7,12 @@ using UnityEngine.UI;
 public class PopUpScript : MonoBehaviour
 {
     [SerializeField] private GameObject Panel;
-    // [SerializeField] private Button retryButton;
-    // [SerializeField] private Button gameQuitButton;
-
-    // [SerializeField] private int currentLives;
-
-    // private bool isGameOver = false;
 
     void Start()
     {
         Panel.SetActive(false);
-        // currentLives = FindFirstObjectByType<CharacterScript>().lives;
     }
 
-    // public void UpdateLives(){
-    //     currentLives = FindFirstObjectByType<CharacterScript>().lives;
-    //     if(currentLives == 0){
-    //         gameOverPanel.SetActive(true);
-
-    //     // Freeze time
-    //         Time.timeScale = 0f;
-
-    //         retryButton.onClick.AddListener(Retry);
-    //         gameQuitButton.onClick.AddListener(GameOver);
-    //     }
-    // }
     public void ActivatePanel(){
         Panel.SetActive(true);
         Time.timeScale = 0f;
